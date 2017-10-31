ec2bd8763a1ea7f710a43c8b5abb475b6e79da2b
Bess Lin <bess861231@gmail.com>
2017-10-31 12:16:22 +0800

    Toast, AlertDialog

--- app/src/main/java/com/bess/bmi/MainActivity.java
+++ app/src/main/java/com/bess/bmi/MainActivity.java
@@ -1,10 +1,12 @@
 package com.bess.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight / (height * height);
         Log.d("MainActivity", "Your BMI is " + bmi);
+        new AlertDialog.Builder(this)
+                .setMessage("Your BMI is "+bmi)
+                .setTitle("my title")
+                .setPositiveButton("OK", null)
+                .show();
+        /*Toast.makeText(this, "Your BMI is "+bmi, Toast.LENGTH_LONG).show();
+               第三個參數為顯示時間的長短*/
     }
 }
 
