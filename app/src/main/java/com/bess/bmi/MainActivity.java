66e6ff9ae39302bcbfd9724f3ab01b73a660d936
Bess Lin <bess861231@gmail.com>
2017-11-07 11:27:49 +0800

    OnClickListener

--- app/src/main/java/com/bess/bmi/MainActivity.java
+++ app/src/main/java/com/bess/bmi/MainActivity.java
@@ -11,19 +11,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String s = new String("abc");
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
 
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        getResources().getString(R.string.app_name);
         Button bHelp = (Button) findViewById(R.id.b_help);
-        bHelp.setOnClickListener(listener);
+        bHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("說明")
+                        .setMessage("BMI原來的設計是一個用於公眾健康研究的統計工具。當需要知道肥胖是否為某一疾病的致病原因時，可以把病人的身高及體重換算成BMI，再找出其數值及病發率是否有線性關連。")
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+        });
     }
     public void bmi(View view){
 //        System.out.println("what???");
