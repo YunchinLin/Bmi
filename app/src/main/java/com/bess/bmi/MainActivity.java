3f425074f39341085b7ead5b555e461b179f52e4
Bess Lin <bess861231@gmail.com>
2017-11-21 12:05:04 +0800

    ResultActivity

--- app/src/main/java/com/bess/bmi/MainActivity.java
+++ app/src/main/java/com/bess/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bess.bmi;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,13 +12,18 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String s = new String("abc");
+    private EditText edWeight;//屬性
+    private EditText edHeight;
+    private Button bHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*EditText edWeight = (EditText) findViewById(R.id.ed_weight);//宣告、定義
+        EditText edHeight = (EditText) findViewById(R.id.ed_height);*/
+        findViews();
 //        getResources().getString(R.string.app_name);
-        Button bHelp = (Button) findViewById(R.id.b_help);
         bHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,16 +35,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume");
+    }
+
+    private void findViews() {
+        edWeight = (EditText) findViewById(R.id.ed_weight);
+        edHeight = (EditText) findViewById(R.id.ed_height);
+        bHelp = (Button) findViewById(R.id.b_help);
+    }
+
     public void bmi(View view){
 //        System.out.println("what???");
         Log.d("MainActivity", "testing bmi method");
-        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
-        EditText edHeight = (EditText) findViewById(R.id.ed_height);
+        /*EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+        EditText edHeight = (EditText) findViewById(R.id.ed_height);*/
 //        String s = edWeight.getText().toString();
         float weight = Float.parseFloat(edWeight.getText().toString());
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight / (height * height);
-        Log.d("MainActivity", "Your BMI is " + bmi);
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("EXTRA_BMI", bmi);
+        startActivity(intent);
+
+        /*Log.d("MainActivity", "Your BMI is " + bmi);
         if (height > 3){
             new AlertDialog.Builder(this)
                     .setMessage("身高單位應為公尺")
@@ -54,9 +102,11 @@ public class MainActivity extends AppCompatActivity {
                     .setTitle(R.string.bmi_title)
                     .setPositiveButton(R.string.ok, null)
                     .show();
-        }
+        }*/
         /*Toast.makeText(this, "Your BMI is "+bmi, Toast.LENGTH_LONG).show();
                第三個參數為顯示時間的長短*/
     }
 }
+
+
 
